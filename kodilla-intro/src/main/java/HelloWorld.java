204411c8983cf52b0315c44970686466ca9c3323
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-03-28 18:36:56 +0200

    First task: Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        // 1a
+        System.out.println("Hello world");
+        System.out.println("Nazywam się Konrad");
+        System.out.println("Mam 24 lata");
+        System.out.println("K");
+
+        // 1b
+       // String mojeImie = "Konrad";
+        //System.out.println("Nazywam się: " + mojeImie);
+
+        //1c
+      //  int mojWiek;
+      //  mojWiek = 24;
+      //  System.out.println("Mam: " + mojWiek + " lata");
+
+        //1d
+     //   char pierwszaLitera;
+     //   pierwszaLitera = 'K';
+     //   System.out.println(pierwszaLitera);
+    }
+}
