774da50c6947b886482e10b71b51da89229a266b
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-04-21 22:35:07 +0200

    Task: Read attributes

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,40 @@
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!"); // [3]
+import com.kodilla.Notebook;
+
+import java.util.Scanner;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass!");
+
+
+        Notebook notebook = new Notebook(600, 3000, 2022, 16, "Asus");
+        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year + " " + notebook.ram + " " + notebook.model);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkPriceandYear();
+        notebook.checkRam();
+
+        Notebook heavyNotebook = new Notebook(2000, 1200, 2019, 8, "Accer");
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + heavyNotebook.year + " " + heavyNotebook.ram + " " + heavyNotebook.model);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkPriceandYear();
+        heavyNotebook.checkRam();
+
+        Notebook oldNotebook = new Notebook(1200, 500, 2017, 8, "HP");
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year + " " + oldNotebook.ram + " " + oldNotebook.model);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkPriceandYear();
+        oldNotebook.checkRam();
+
+        Notebook newNotebook = new Notebook(1100, 2000, 2023, 64, "Lenovo");
+        System.out.println(newNotebook.weight + " " + newNotebook.price + " " + newNotebook.year + " " + newNotebook.ram + " " + newNotebook.model);
+        newNotebook.checkWeight();
+        newNotebook.checkPrice();
+        newNotebook.checkPriceandYear();
+        newNotebook.checkRam();
+
+
     }
 }
---
+++ kodilla-intro/src/main/java/com/kodilla/Notebook.java
@@ -0,0 +1,61 @@
+package com.kodilla;
+
+import java.security.PublicKey;
+
+public class Notebook {
+
+    public int weight;
+    public int price;
+    public int year;
+    public int ram;
+    public String model;
+
+
+    public Notebook(int weight, int price, int year, int ram, String model) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+        this.ram = ram;
+        this.model = model;
+    }
+
+    public void checkPrice() {
+        if (this.price > 1000) {
+            System.out.println("This notebook is expensive.");
+        } else if (this.price < 1000 && this.price > 600){
+            System.out.println("The price is good.");
+        } else {
+            System.out.println("This notebook is very cheap.");
+        }
+    }
+
+    public void checkWeight(){
+        if(this.weight > 1500){
+            System.out.println("The notebook is heavy.");
+        } else if (this.weight >= 1000 && this.weight <= 1500) {
+            System.out.println("The weight is good.");
+        } else {
+            System.out.println("This notebook is light.");
+        }
+    }
+
+    public void checkPriceandYear(){
+        if(this.price > 1500 && this.year > 2021){
+            System.out.println("It is very new notebook, but is very expensive.");
+        } else if (this.year >= 2018 && (this.price <= 1500 && this.price >= 1000)){
+            System.out.println("It is good notebook and price is not too high");
+        } else {
+            System.out.println("It is old notebook, but it is very cheap.");
+        }
+    }
+
+    public void checkRam(){
+        if(this.ram >= 32){
+            System.out.println("This notebook is very fast.");
+        } else if (this.ram < 32 || this.ram >= 16) {
+            System.out.println("This notebook is fine.");
+        } else {
+            System.out.println("This notebook is very slow");
+        }
+    }
+}
