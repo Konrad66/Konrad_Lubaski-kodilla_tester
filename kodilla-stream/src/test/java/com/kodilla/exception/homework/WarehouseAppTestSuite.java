6c3be0827a826807fe8f0d2022b9a79a09471ea6
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-07-04 19:40:43 +0200

    Task 6.5: Exceptions and the test

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseAppTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseAppTestSuite.java
@@ -7,27 +7,13 @@ import java.util.function.BooleanSupplier;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseAppTestSuite {
-
-    /*
+    
     @Test
-    public void testWarehouseAppTestSuite() throws OrderDoesntExistException {
-        //given
-        Warehouse warehouse = new Warehouse();
-        // when
-        Order isOrderInWarehouse = warehouse.getOrder("123");
-        //then
-        assertTrue;
-    }
-*/
-
-    @Test
-    public void testWarehouseAppTestSuite_WithException(){
+    public void testWarehouseAppTestSuite_WithException() {
         //givenn
         Warehouse warehouse = new Warehouse();
         //when
         //then
         assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("245"));
     }
-
-
 }
