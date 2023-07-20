a3332490cf8060eefecb8da0aebf9734b145ae0f
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2023-07-20 13:38:20 +0200

    Task 10.5: Corrections

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTestSuite.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.rest.controller;
 
+//import com.google.gson.Gson;
 import com.kodilla.rest.domain.BookDto;
 import com.kodilla.rest.service.BookService;
 import org.hamcrest.Matchers;
@@ -17,6 +18,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(BookController.class)
@@ -39,7 +43,27 @@ public class BookControllerMvcTestSuite {
         //when & then
         mockMvc.perform(MockMvcRequestBuilders.get("/books")                // [1]
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().is(200))               // [2]
+                .andExpect(status().is(200))               // [2]
                 .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
     }
+
+
+/*
+
+    @Test
+    void shouldAddBook() throws Exception{
+        //given
+        Gson gson = new Gson();
+        BookDto exampleBook = new BookDto("Title 1", "Author 1");
+        String json = gson.toJson(exampleBook);
+        Mockito.doNothing().when(bookService).addBook(Mockito.any(BookDto.class));
+
+        //when & then
+        mockMvc.perform(post("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().is(200));
+    }
+
+ */
 }
